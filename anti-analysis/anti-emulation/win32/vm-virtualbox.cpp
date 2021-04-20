4d6ba124845a0626f61a98ed57b498c933b44431
xathrya <me@xathrya.id>
2021-04-21 00:01:34 +0700

    Anti VBox using WMI, bus, and baseboard

--- anti-analysis/anti-emulation/win32/vm-virtualbox.cpp
+++ anti-analysis/anti-emulation/win32/vm-virtualbox.cpp
@@ -10,6 +10,7 @@ Compile:
     $ cl vm-virtualbox.cpp
 */
 #include <windows.h> 
+
 #include "../../utils.common.hpp"
 #include "../../utils.registry.hpp"
 #include "../../utils.fs.hpp"
@@ -34,10 +35,10 @@ bool antivm_vbox_regkeyval ()
     /* Daftar registry key values yang terdapat di lingkungan VirtualBox */
     wchar_t * entries[][3] = {
         // SUBKEY                          VALUE NAME           VALUE
-		{ L"HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", L"Identifier", L"VBOX" },
-		{ L"HARDWARE\\Description\\System", L"SystemBiosVersion", L"VBOX" },
-		{ L"HARDWARE\\Description\\System", L"VideoBiosVersion",  L"VIRTUALBOX" },
-		{ L"HARDWARE\\Description\\System", L"SystemBiosDate",    L"06/23/99" },
+        { L"HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", L"Identifier", L"VBOX" },
+        { L"HARDWARE\\Description\\System", L"SystemBiosVersion", L"VBOX" },
+        { L"HARDWARE\\Description\\System", L"VideoBiosVersion",  L"VIRTUALBOX" },
+        { L"HARDWARE\\Description\\System", L"SystemBiosDate",    L"06/23/99" },
     };
 
     size_t length = sizeof(entries) / sizeof(entries[0]);
@@ -58,15 +59,15 @@ bool antivm_vbox_regkey()
 {
     /* Daftar registry key yang terdapat di lingkungan VirtualBox */
     wchar_t * entries[] = {
-		L"HARDWARE\\ACPI\\DSDT\\VBOX__",
-		L"HARDWARE\\ACPI\\FADT\\VBOX__",
-		L"HARDWARE\\ACPI\\RSDT\\VBOX__",
-		L"SOFTWARE\\Oracle\\VirtualBox Guest Additions",
-		L"SYSTEM\\ControlSet001\\Services\\VBoxGuest",
-		L"SYSTEM\\ControlSet001\\Services\\VBoxMouse",
-		L"SYSTEM\\ControlSet001\\Services\\VBoxService",
-		L"SYSTEM\\ControlSet001\\Services\\VBoxSF",
-		L"SYSTEM\\ControlSet001\\Services\\VBoxVideo"
+        L"HARDWARE\\ACPI\\DSDT\\VBOX__",
+        L"HARDWARE\\ACPI\\FADT\\VBOX__",
+        L"HARDWARE\\ACPI\\RSDT\\VBOX__",
+        L"SOFTWARE\\Oracle\\VirtualBox Guest Additions",
+        L"SYSTEM\\ControlSet001\\Services\\VBoxGuest",
+        L"SYSTEM\\ControlSet001\\Services\\VBoxMouse",
+        L"SYSTEM\\ControlSet001\\Services\\VBoxService",
+        L"SYSTEM\\ControlSet001\\Services\\VBoxSF",
+        L"SYSTEM\\ControlSet001\\Services\\VBoxVideo"
     };
 
     size_t length = sizeof(entries) / sizeof(entries[0]);
@@ -85,26 +86,26 @@ bool antivm_vbox_regkey()
  */
 bool antivm_vbox_files()
 {
-	/* Daftar path yang terdapat di lingkungan VirtualBox */
-	wchar_t * entries[] = {
-		L"system32\\drivers\\VBoxMouse.sys",
-		L"system32\\drivers\\VBoxGuest.sys",
-		L"system32\\drivers\\VBoxSF.sys",
-		L"system32\\drivers\\VBoxVideo.sys",
-		L"system32\\vboxdisp.dll",
-		L"system32\\vboxhook.dll",
-		L"system32\\vboxmrxnp.dll",
-		L"system32\\vboxogl.dll",
-		L"system32\\vboxoglarrayspu.dll",
-		L"system32\\vboxoglcrutil.dll",
-		L"system32\\vboxoglerrorspu.dll",
-		L"system32\\vboxoglfeedbackspu.dll",
-		L"system32\\vboxoglpackspu.dll",
-		L"system32\\vboxoglpassthroughspu.dll",
-		L"system32\\vboxservice.exe",
-		L"system32\\vboxtray.exe",
-		L"system32\\VBoxControl.exe",
-	};
+    /* Daftar path yang terdapat di lingkungan VirtualBox */
+    wchar_t * entries[] = {
+        L"system32\\drivers\\VBoxMouse.sys",
+        L"system32\\drivers\\VBoxGuest.sys",
+        L"system32\\drivers\\VBoxSF.sys",
+        L"system32\\drivers\\VBoxVideo.sys",
+        L"system32\\vboxdisp.dll",
+        L"system32\\vboxhook.dll",
+        L"system32\\vboxmrxnp.dll",
+        L"system32\\vboxogl.dll",
+        L"system32\\vboxoglarrayspu.dll",
+        L"system32\\vboxoglcrutil.dll",
+        L"system32\\vboxoglerrorspu.dll",
+        L"system32\\vboxoglfeedbackspu.dll",
+        L"system32\\vboxoglpackspu.dll",
+        L"system32\\vboxoglpassthroughspu.dll",
+        L"system32\\vboxservice.exe",
+        L"system32\\vboxtray.exe",
+        L"system32\\VBoxControl.exe",
+    };
 
     size_t length = sizeof(entries) / sizeof(entries[0]);
 
@@ -130,7 +131,7 @@ bool antivm_vbox_files()
  */
 bool antivm_vbox_dirs()
 {
-	wchar_t programfiles[MAX_PATH];
+    wchar_t programfiles[MAX_PATH];
     wchar_t path[MAX_PATH];
     wchar_t target[MAX_PATH] = L"oracle\\virtualbox guest additions\\";
 
@@ -162,11 +163,11 @@ bool antivm_vbox_macaddr ()
 bool antivm_vbox_devices ()
 {
     wchar_t * entries[] = {
-		L"\\\\.\\VBoxMiniRdrDN",
-		L"\\\\.\\VBoxGuest",
-		L"\\\\.\\pipe\\VBoxMiniRdDN",
-		L"\\\\.\\VBoxTrayIPC",
-		L"\\\\.\\pipe\\VBoxTrayIPC"
+        L"\\\\.\\VBoxMiniRdrDN",
+        L"\\\\.\\VBoxGuest",
+        L"\\\\.\\pipe\\VBoxMiniRdDN",
+        L"\\\\.\\VBoxTrayIPC",
+        L"\\\\.\\pipe\\VBoxTrayIPC"
     };
     bool found = false;
 
@@ -219,8 +220,8 @@ bool antivm_vbox_network_share ()
 bool antivm_vbox_process ()
 {
     wchar_t * entries [] = {
-		L"vboxservice.exe",
-		L"vboxtray.exe"
+        L"vboxservice.exe",
+        L"vboxtray.exe"
     };
 
     size_t length = sizeof(entries) / sizeof(entries[0]);
@@ -417,6 +418,364 @@ bool antivm_vbox_wmi_evtlog ()
     return found;
 }
 
+/**
+ *  Periksa PnP (Plug and Play) entity untuk PCI Device
+ */
+bool antivm_vbox_wmi_pnpentity_pcideviceid()
+{
+    IWbemServices * svc = NULL;
+    IWbemLocator  * loc = NULL;
+    IEnumWbemClassObject * enumerator = NULL;
+    HRESULT result;
+    bool    found = false;
+
+    // Inisialisasi WMI
+    if (InitWMI(&svc, &loc, L"ROOT\\CIMV2"))
+    {
+        // eksekusi Query
+        if (ExecWMIQuery(&svc, &loc, &enumerator, L"SELECT * FROM Win32_PnPEntity"))
+        {
+            IWbemClassObject * iter = NULL;
+            ULONG   retval = 0;
+            VARIANT vtProp;
+
+            while (enumerator && !found)
+            {
+                result = enumerator->Next(WBEM_INFINITE, 1, &iter, &retval);
+                if (retval == 0) break;
+
+                // dapatkan value dari property Name
+                result = iter->Get(L"DeviceId", 0, &vtProp, 0, 0);
+
+                if (SUCCEEDED(result)) 
+                {
+                    if (vtProp.vt == VT_BSTR) 
+                    {
+                        if (wcsstr(vtProp.bstrVal, L"PCI\\VEN_80EE&DEV_CAFE") != 0)
+                            found = true;
+                    }
+                    VariantClear(&vtProp);
+                }
+                
+                iter->Release();
+            }
+
+            // cleanup
+            enumerator->Release();
+            svc->Release();
+            loc->Release();
+            CoUninitialize();
+        }
+    }
+
+    return found;
+}
+
+/**
+ *  Periksa PnP (Plug n Play) entity untuk hardware VirtualBox
+ */
+bool antivm_vbox_wmi_pnpentity_controllers()
+{
+    IWbemServices * svc = NULL;
+    IWbemLocator  * loc = NULL;
+    IEnumWbemClassObject * enumerator = NULL;
+    HRESULT result;
+    bool    found = false;
+
+    // Inisialisasi WMI
+    if (InitWMI(&svc, &loc, L"ROOT\\CIMV2"))
+    {
+        // eksekusi Query
+        if (ExecWMIQuery(&svc, &loc, &enumerator, L"SELECT * FROM Win32_PnPEntity"))
+        {
+            // Get the data from the query
+            IWbemClassObject * iter = NULL;
+            ULONG   retval = 0;
+            VARIANT vtProp;
+
+            int findCount = 0;
+            const int findThreshold = 3;
+
+            while (enumerator)
+            {
+                result = enumerator->Next(WBEM_INFINITE, 1, &iter, &retval);
+                if (retval == 0) break;
+
+                // dapatkan value dari property Name
+                result = iter->Get(L"Name", 0, &vtProp, 0, 0);
+                if (SUCCEEDED(result)) 
+                {
+                    if (V_VT(&vtProp) != VT_NULL)
+                    {
+                        if ((vtProp.vt & VT_BSTR) == VT_BSTR)
+                        {
+                            // naikkan counter jika instance cocok dengan nama VBox
+                            if (wcsstr(vtProp.bstrVal, L"82801FB") != 0)
+                                findCount++;
+
+                            else if (wcsstr(vtProp.bstrVal, L"82441FX") != 0)
+                                findCount++;
+                                
+                            else if (wcsstr(vtProp.bstrVal, L"82371SB") != 0)
+                                findCount++;
+
+                            else if (wcsstr(vtProp.bstrVal, L"OpenHCD") != 0)
+                                findCount++;
+                        }
+
+                        VariantClear(&vtProp);
+                    }
+                }
+                iter->Release();
+            }
+
+            if (findCount >= findThreshold)
+                found = true;
+
+            // Cleanup
+            enumerator->Release();
+            svc->Release();
+            loc->Release();
+            CoUninitialize();
+        }
+    }
+
+    return found;
+}
+
+/**
+ *  periksa PnP (Plug n Play) untuk device VBOX
+ */
+bool antivm_vbox_wmi_pnpentity_vboxname()
+{
+    IWbemServices * svc = NULL;
+    IWbemLocator  * loc = NULL;
+    IEnumWbemClassObject * enumerator = NULL;
+    HRESULT result;
+    bool    found = false;
+
+    // Inisialisasi WMI
+    if (InitWMI(&svc, &loc, L"ROOT\\CIMV2"))
+    {
+        // eksekusi Query
+        if (ExecWMIQuery(&svc, &loc, &enumerator, L"SELECT * FROM Win32_PnPDevice"))
+        {
+            IWbemClassObject * iter = NULL;
+            ULONG   retval = 0;
+            VARIANT vtProp;
+
+            while (enumerator && !found)
+            {
+                result = enumerator->Next(WBEM_INFINITE, 1, &iter, &retval);
+                if (retval == 0) break;
+
+                // dapatkan value dari property Name
+                result = iter->Get(L"Name", 0, &vtProp, 0, 0);
+                if (SUCCEEDED(result))
+                {
+                    if (V_VT(&vtProp) != VT_NULL)
+                    {
+                        if ((vtProp.vt & VT_BSTR) == VT_BSTR)
+                        {
+                            if (wcsstr(vtProp.bstrVal, L"VBOX") != 0)
+                                found = true;
+                        }
+
+                        VariantClear(&vtProp);
+                    }
+                }
+
+                // dapatkan value dari property Caption
+                result = iter->Get(L"Caption", 0, &vtProp, 0, 0);
+                if (SUCCEEDED(result))
+                {
+                    if (V_VT(&vtProp) != VT_NULL)
+                    {
+                        if ((vtProp.vt & VT_BSTR) == VT_BSTR)
+                        {
+                            if (wcsstr(vtProp.bstrVal, L"VBOX") != 0)
+                                found = true;
+                        }
+
+                        VariantClear(&vtProp);
+                    }
+                }
+
+                // dapatkan value dari property PNPDeviceID
+                result = iter->Get(L"PNPDeviceID", 0, &vtProp, 0, 0);
+                if (SUCCEEDED(result))
+                {
+                    if (V_VT(&vtProp) != VT_NULL)
+                    {
+                        if ((vtProp.vt & VT_BSTR) == VT_BSTR)
+                        {
+                            if (wcsstr(vtProp.bstrVal, L"VEN_VBOX") != 0)
+                                found = true;
+                        }
+
+                        VariantClear(&vtProp);
+                    }
+                }
+
+                iter->Release();
+            }
+
+            // Cleanup
+            enumerator->Release();
+            svc->Release();
+            loc->Release();
+            CoUninitialize();
+        }
+    }
+
+    return found;
+}
+
+/**
+ *  periksa bus apakah hanya terdapat ACPIBus_BUS_0, PCI_BUS_0, PNP_BUS_0
+ */
+bool antivm_vbox_bus()
+{
+    IWbemServices * svc = NULL;
+    IWbemLocator  * loc = NULL;
+    IEnumWbemClassObject * enumerator = NULL;
+    HRESULT result;
+    bool    found = false;
+
+    // Inisialisasi WMI
+    if (InitWMI(&svc, &loc, L"ROOT\\CIMV2"))
+    {
+        // eksekusi Query
+        if (ExecWMIQuery(&svc, &loc, &enumerator, L"SELECT * FROM Win32_Bus"))
+        {
+            IWbemClassObject * iter = NULL;
+            ULONG   retval = 0;
+            VARIANT vtProp;
+            
+            int count = 0;
+            int findCount = 0;
+            const int findThreshold = 3;
+
+            while (enumerator)
+            {
+                result = enumerator->Next(WBEM_INFINITE, 1, &iter, &retval);
+                if (retval == 0) break;
+
+                count++;
+
+                // dapatkan value dari property Name
+                result = iter->Get(L"Name", 0, &vtProp, 0, 0);
+                if (SUCCEEDED(result)) 
+                {
+                    if (V_VT(&vtProp) != VT_NULL)
+                    {
+                        if ((vtProp.vt & VT_BSTR) == VT_BSTR)
+                        {
+                            if (wcsstr(vtProp.bstrVal, L"ACPIBus_BUS_0") != 0)
+                                findCount++;
+
+                            else if (wcsstr(vtProp.bstrVal, L"PCI_BUS_0") != 0)
+                                findCount++;
+
+                            else if (wcsstr(vtProp.bstrVal, L"PNP_BUS_0") != 0)
+                                findCount++;
+                        }
+                        
+                        VariantClear(&vtProp);
+                    }
+                }
+                iter->Release();
+            }
+
+            // periksa jika terdapat N instance yang cocok dengan string di atas
+            if (count == findThreshold && findCount == findThreshold)
+                found = true;
+
+            // Cleanup
+            enumerator->Release();
+            svc->Release();
+            loc->Release();
+            CoUninitialize();
+        }
+    }
+
+    return found;
+}
+
+/**
+ *  Periksa baseboard
+ */
+bool antivm_vbox_wmi_baseboard()
+{
+    IWbemServices * svc = NULL;
+    IWbemLocator  * loc = NULL;
+    IEnumWbemClassObject * enumerator = NULL;
+    HRESULT result;
+    bool    found = false;
+
+    // Inisialisasi WMI
+    if (InitWMI(&svc, &loc, L"ROOT\\CIMV2"))
+    {
+        // eksekusi Query
+        if (ExecWMIQuery(&svc, &loc, &enumerator, L"SELECT * FROM Win32_BaseBoard"))
+        {
+            IWbemClassObject * iter = NULL;
+            ULONG   retval = 0;
+            VARIANT vtProp = { 0 };
+
+            while (enumerator && !found)
+            {
+                result = enumerator->Next(WBEM_INFINITE, 1, &iter, &retval);
+                if (retval == 0) break;
+
+                // dapatkan value dari property Product
+                result = iter->Get(L"Product", 0, &vtProp, 0, 0);
+                if (SUCCEEDED(result))
+                {
+                    if (V_VT(&vtProp) != VT_NULL)
+                    {
+                        if ((vtProp.vt & VT_BSTR) == VT_BSTR)
+                        {
+                            if (wcsstr(vtProp.bstrVal, L"VirtualBox") != 0)
+                                found = true;
+                        }
+
+                        VariantClear(&vtProp);
+                    }
+                }
+
+                vtProp = { 0 };
+
+                // dapatkan value dari property Manufacturer
+                result = iter->Get(L"Manufacturer", 0, &vtProp, 0, 0);
+                if (SUCCEEDED(result))
+                {
+                    if (V_VT(&vtProp) != VT_NULL)
+                    {
+                        if ((vtProp.vt & VT_BSTR) == VT_BSTR)
+                        {
+                            if (wcsstr(vtProp.bstrVal, L"Oracle Corporation") != 0)
+                                found = true;
+                        }
+
+                        VariantClear(&vtProp);
+                    }
+                }
+
+                iter->Release();
+            }
+
+            // Cleanup
+            enumerator->Release();
+            svc->Release();
+            loc->Release();
+            CoUninitialize();
+        }
+    }
+
+    return found;
+}
+
 
 /**
  *  Periksa firmware SMBIOS
@@ -496,6 +855,11 @@ int main(int argc, char* argv[])
     check(antivm_vbox_wmi,              "VM VBox : WMI (Device)");
     check(antivm_vbox_wmi_mac,          "VM VBox : WMI (MAC Address)");
     check(antivm_vbox_wmi_evtlog,       "VM VBox : WMI (Event Log)");
+    check(antivm_vbox_wmi_pnpentity_pcideviceid, "VM VBox : WMI PnPEntity PCIDeviceID");
+    check(antivm_vbox_wmi_pnpentity_controllers, "VM VBox : WMI PnPEntity Controllers");
+    check(antivm_vbox_wmi_pnpentity_vboxname,    "VM VBox : WMI PnPEntity VBoxName");
+    check(antivm_vbox_bus,              "VM VBox : BUS name");
+    check(antivm_vbox_wmi_baseboard,    "VM VBox : BaseBoard");
     check(antivm_vbox_firmware_smbios,  "VM VBox : Firmware BIOS");
     check(antivm_vbox_firmware_acpi,    "VM VBox : Firmware ACPI");
     
